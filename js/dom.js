2c9eb8ddafd196d3853eefd7f037bd2ee5e883a2
Michael Tobin <mikel.tobin@gmail.com>
2016-06-06 15:41:08 -0600

    Finished placing the 3 pictures on the splash page in the correct spot on the page. They also call the API and randomly choose three to appear on the splash page.

---
+++ js/dom.js
@@ -0,0 +1,42 @@
+var userClick = document.getElementsByClassName('trackButton')[0];
+var httpRequest = new XMLHttpRequest();
+var topPictureGet = document.getElementById('topPicture');
+var imageGet = document.getElementsByClassName('pics');
+var imageArr = ['ghost_in_the_machine.jpg','thriller.jpg','red.jpg','21.jpg','the_division_bell'];
+
+window.onLoad= pageLoad();
+
+function pageLoad(){
+  httpRequest.onreadystatechange =function(){
+    if(httpRequest.readyState === 4){
+      if(httpRequest.status < 400){
+        var jsonNewObj=JSON.parse(httpRequest.responseText);
+          updateDOM(randomizePics(jsonNewObj));
+
+        if(jsonNewObj.Response ==="False"){
+        }
+      }
+    }
+  }
+  httpRequest.open('GET', 'https://lit-fortress-6467.herokuapp.com/object');
+  httpRequest.send();
+};
+
+function updateDOM(arrOfArrays){
+  for (var i = 0; i < arrOfArrays.length; i++) {
+    if(arrOfArrays[i] ==  "the_division_bell.jpg"){
+      imageGet[i].src = 'images/division_bell.jpg';
+      updateImage();
+    }else{
+      imageGet[i].src = 'images/'+arrOfArrays[i];
+      updateImage();
+    }
+  }
+}
+
+function updateImage(){
+  for (var i = 0; i < imageGet.length; i++) {
+    imageGet[i].style.width = '100%';
+    imageGet[i].style.height = '100%';
+  }
+}
---
+++ js/logic.js
@@ -0,0 +1,18 @@
+  function randomizePics(jsonObj){
+    var newArr= [];
+    for (var i = 0; i < jsonObj.results.length; i++) {
+      newArr[i]= jsonObj.results[i].cover_art;
+    }
+    return chooseRandom(newArr);
+};
+
+function chooseRandom(arr){
+  // // create random number from 0 to length of array
+  // var random = Math.floor(Math.random()*arr.length)
+  // return arr[4]
+  newArr1 = [];
+  for (var i = 0; i < 3; i++) {
+    newArr1[i] = (arr.splice([Math.floor(Math.random()*arr.length)],1));
+  }
+  return newArr1;
+}
