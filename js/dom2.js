764bba9db2f191d42f8d97ea557dad3e156e984f
Michael Tobin <mikel.tobin@gmail.com>
2016-06-07 12:07:36 -0600

    Working top picture bar. Updates with the length of the given array. For Nmutas API it displays the 5 album covers. But when converting to Spotify API it will update with the given length of tracks requested.

--- js/dom2.js
+++ js/dom2.js
@@ -2,6 +2,8 @@ var pictureDivToAppend = $('.picture_container')[0];
 var buttonToClickActionTEMP = $('.actionButtons')[0];
 var imagePrefab = $('.topBarImages');
 var httpRequest = new XMLHttpRequest();
+var imageHolder = [];
+var globalArrayHolder;
 window.onLoad= pageLoad();
 
 function pageLoad(){
@@ -9,7 +11,8 @@ function pageLoad(){
     if(httpRequest.readyState === 4){
       if(httpRequest.status < 400){
         var jsonNewObj=JSON.parse(httpRequest.responseText);
-          chooseRandom(make_JSON_Object_Array(jsonNewObj),3);
+          globalArrayHolder = make_JSON_Object_Array(jsonNewObj);
+          updateDOM(globalArrayHolder);
         if(jsonNewObj.Response ==="False"){
         }
       }
@@ -19,18 +22,22 @@ function pageLoad(){
   httpRequest.send();
 };
 
-
-function updateDOM(){
-  var img_Inner = $("<img></img>", {class: "topBarImages"});
-  $(pictureDivToAppend).append(img_Inner);
+function updateDOM(arr){
+  for (var i = 0; i < arr.length; i++) {
+    var img_Inner = $("<img></img>", {class: "topBarImages", src:('images/'+arr[i])});
+    $(pictureDivToAppend).append(img_Inner);
+    pushImageArray(img_Inner);
+  }
 
 }
 
-$('.topBarImages').click(function(){
-  alert(this.src);
+function pushImageArray(newImage){
+  imageHolder.push(newImage);
+}
+
+$('.topBarImages').click(function(ele){
+
 })
 
 $(buttonToClickActionTEMP).click(function(){
-  updateDOM();
-
 })
