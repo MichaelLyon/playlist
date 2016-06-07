7f8c90a46e85c03ba650199f5926882520bf624d
Michael Tobin <mikel.tobin@gmail.com>
2016-06-07 14:29:54 -0600

    Commiting working pictures bar using Jquery and an event listener to the pictures that updates the box div to display the album cover file type and name. Working on transfering that to the artist name next

--- js/dom2.js
+++ js/dom2.js
@@ -1,5 +1,6 @@
 var pictureDivToAppend = $('.picture_container')[0];
 var buttonToClickActionTEMP = $('.actionButtons')[0];
+var boxToAppendTo = $('.actualBox')[0];
 var imagePrefab = $('.topBarImages');
 var httpRequest = new XMLHttpRequest();
 var imageHolder = [];
@@ -13,6 +14,13 @@ function pageLoad(){
         var jsonNewObj=JSON.parse(httpRequest.responseText);
           globalArrayHolder = make_JSON_Object_Array(jsonNewObj);
           updateDOM(globalArrayHolder);
+          $(pictureDivToAppend.childNodes).click(function(ele){
+            for (var i = 0; i < imageHolder.length; i++) {
+              if(this.id === imageHolder[i][0].id){
+                updateBoxDiv(globalArrayHolder[i]);
+              }
+            }
+          })
         if(jsonNewObj.Response ==="False"){
         }
       }
@@ -24,20 +32,20 @@ function pageLoad(){
 
 function updateDOM(arr){
   for (var i = 0; i < arr.length; i++) {
-    var img_Inner = $("<img></img>", {class: "topBarImages", src:('images/'+arr[i])});
+    var img_Inner = $("<img></img>", {class: "topBarImages",id:(i), src:('images/'+arr[i])});
     $(pictureDivToAppend).append(img_Inner);
     pushImageArray(img_Inner);
   }
+}
 
+function updateBoxDiv(ele){
+  var newBoxDiv = $("<div></div>", {class:'artistDiv'}).text(ele);
+  $(boxToAppendTo).append(newBoxDiv);
 }
 
 function pushImageArray(newImage){
   imageHolder.push(newImage);
 }
-
-$('.topBarImages').click(function(ele){
-
-})
 
 $(buttonToClickActionTEMP).click(function(){
 })
